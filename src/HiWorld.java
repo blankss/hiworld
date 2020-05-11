5b2e75b74525f7d4fc7ba27c7f3f6af5e9412b93
blankss <34908156+blankss@users.noreply.github.com>
2020-05-11 23:52:50 +0800

    test

---
+++ src/HiWorld.java
@@ -0,0 +1,15 @@
+/**
+ * PackgeName: PACKAGE_NAME
+ * Author: ppz
+ * Date: 2020/5/11 23:50
+ * project name: hiword
+ * Version:
+ * Description:
+ */
+
+
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi World");
+    }
+}
